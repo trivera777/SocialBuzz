91cd6c56785561b22ac2c980966618e8a317acd0
Tifanny Rivera <trivera51580@gmail.com>
2022-01-01 15:48:42 -0700

    when user is logged in and creates a post, their username is in green text

--- socialmediareact/src/App.js
+++ socialmediareact/src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
 
+export const UserContext = React.createContext();
+
 function App() {
   const [user, setUser] = React.useState("");
   const [posts, setPosts] = React.useState([]);
@@ -12,20 +14,23 @@ function App() {
     document.title = user ? `${user}'s Feed` : "Please login.";
   }, [user]);
 
-  function handleAddPost(newPost){
-    setPosts([newPost, ...posts])
-  }
+  const handleAddPost = React.useCallback(
+    (newPost) => {
+      setPosts([newPost, ...posts]);
+    },
+    [posts]
+  );
 
   if (!user) {
     return <Login setUser={setUser} />;
   }
 
   return (
-    <>
+    <UserContext.Provider value={user}>
       <Header user={user} setUser={setUser} />
       <CreatePost user={user} handleAddPost={handleAddPost} />
-      <PostList posts={posts} />
-    </>
+      <PostList  posts={posts} />
+    </UserContext.Provider>
   );
 }
 
--- socialmediareact/src/components/Post.js
+++ socialmediareact/src/components/Post.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { UserContext } from '../App'
 
 function Post({ image, content, user }) {
   return (
-    <>
-      {image && (
-        <img
-          style={{ height: 100, width: 200, objectFit: "cover" }}
-          src={URL.createObjectURL(image)}
-          alt="Post cover"
-        />
+    <UserContext.Consumer>
+      {currentUser => (
+        <>
+          {image && (
+            <img
+              style={{ height: 100, width: 200, objectFit: "cover" }}
+              src={URL.createObjectURL(image)}
+              alt="Post cover"
+            />
+          )}
+          <p>{content}</p>
+          <div style={{ color: currentUser === user && "green" }}>{user}</div>
+        </> 
       )}
-      <p>{content}</p>
-      <div>{user}</div>
-    </>
+    </UserContext.Consumer>
   );
 }
 
 export default Post;
- 
