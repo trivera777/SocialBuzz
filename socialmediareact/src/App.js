b187ba488c8ebe9b014ec679f71d2f0d38319ff6
Tifanny Rivera <trivera51580@gmail.com>
2022-01-01 14:01:46 -0700

    posts now show post content and user name

--- socialmediareact/src/App.js
+++ socialmediareact/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react' 
 import Login from './components/Login'
 import Header from './components/Header'
+import CreatePost from './components/CreatePost'
+
 function App(){
   const [user, setUser] = React.useState('');
+  const [posts, setPosts] = React.useState([])
 
   React.useEffect(() => {
     document.title = user ? `${user}'s Feed` : 'Please login.'
@@ -15,6 +18,20 @@ function App(){
   return(
     <>
     <Header user={user} setUser={setUser} />
+    <CreatePost user={user} setPosts={setPosts} posts={posts}/>
+    {posts.map((post, i) => (
+      <React.Fragment key={i}>
+      {post.image && (
+        <img 
+        style={{ height: 100, width: 200, objectFit: 'cover' }}
+        src={URL.createObjectURL(post.image)}
+        alt="Post cover"
+        />
+        )}
+        <p>{post.content}</p>
+        <div>{user}</div>
+      </React.Fragment>
+    ))}
     </>
   )
 }
--- socialmediareact/src/components/CreatePost.js
+++ socialmediareact/src/components/CreatePost.js
@@ -1,8 +1,32 @@
-import React from 'react' 
+import React from "react";
 
-function CreatePost(){
-  return <div>create post</div>
-  
+function CreatePost({ user, setPosts, posts }) {
+  const [content, setContent] = React.useState("");
+  const [image, setImage] = React.useState(null);
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const post = { content, image, user };
+    const newPosts = [post, ...posts];
+    setPosts(newPosts);
+  }
+  return (
+    <div>
+      <h1>Create New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add post content"
+          onChange={(event) => setContent(event.target.value)}
+        />
+        <input
+          type="file"
+          onChange={(event) => setImage(event.target.files[0])}
+        />
+        <button type="submit">Submit Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
